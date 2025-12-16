c100b650db8f838d11e8f384d1dd8a6a01e89926
bala-amypo <bala@amypo.in>
2025-12-16 17:37:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,12 +1,13 @@
 package com.example.demo.entity;
 
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 @Entity
+
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
